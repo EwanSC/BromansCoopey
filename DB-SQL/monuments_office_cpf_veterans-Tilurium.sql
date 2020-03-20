aaf9852874628c8d1e6bc0b7f0b7ad3f2ecad033
EwanSC <ewan.coopey@hdr.mq.edu.au>
2020-03-20 11:14:27 +1100

    Made new view
    Made new view for legionary info in Dalmatia, and saved the .sql query.

---
+++ DB-SQL/monuments_office_cpf_veterans-Tilurium.sql
@@ -0,0 +1,8 @@
+SELECT MonumentID as 'Monument', CorpusName as 'Primary', Reference, MonumentType, militarystatus, OfficeType as 'Active Office', MemberSeventhLegion as 'Member of Legio VII', CPFTitle as 'CPF title'
+FROM Monument JOIN FindSpot USING (FindSpotID)
+              JOIN MonumentMilitaryOffice USING (monumentid)
+			  JOIN MonumentCorpus USING (MonumentID)
+WHERE province = 'Dalmatia' 
+  AND isPrimaryReference = '1' 
+  AND (MonumentType = 'stela' or MonumentType = 'funerary inscription' or MonumentType = 'titulus')
+  ;
