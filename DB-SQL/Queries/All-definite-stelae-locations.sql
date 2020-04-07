5e7884d74ded6f307b8b06d7e4fdba9c11701e6b
EwanSC <ewan.coopey@hdr.mq.edu.au>
2020-04-07 18:27:04 +1000

    Minor edits and queries
    'Fixed' some location data for entries from Aequum, Narona and Iader.
    Made a number of Sql queries, but deleted most of them after editing them

---
+++ DB-SQL/Queries/All-definite-stelae-locations.sql
@@ -0,0 +1,12 @@
+select CorpusName ||' '|| Reference as 'Reference', SpecificLocation as 'Site', Settlement as 'Ancient Settlement', Province
+from Monument 
+join FindSpot using (FindSpotID)
+join MonumentCorpus using (MonumentID)
+where Province = 'Dalmatia'
+and MemberSeventhLegion = 'yes'
+and isPrimaryReference = '1'
+and (MonumentType = 'altar'
+or MonumentType = 'stela'
+or MonumentType = 'inscription fragment'
+or MonumentType = 'funerary inscription')
+order by Settlement;
