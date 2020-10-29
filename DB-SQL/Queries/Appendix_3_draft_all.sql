e24f055b3e1864679d172a384e49d5d86301f845
EwanSC <ewan.coopey@hdr.mq.edu.au>
2020-10-29 12:44:32 +1100

    Bib editing and Appendixing

--- DB-SQL/Queries/Appendix_3_draft_all.sql
+++ DB-SQL/Queries/Appendix_3_draft_all.sql
@@ -1,13 +1,11 @@
-SELECT (Province ||', '|| coalesce(Settlement, '')) AS 'Site', COUNT (*) AS 'Number of Monuments', SUM(COUNT(Settlement)) OVER() AS Total
+SELECT (Province ||', '|| coalesce(Settlement, '')) AS 'Site', COUNT (*) AS 'Number of Monuments', SUM(COUNT(Province)) OVER() AS Total
 FROM FindSpot
 JOIN Monument USING (FindSpotID) 
 WHERE Province = 'Dalmatia'
-AND (MemberSeventhLegion = 'yes'
-OR MemberSeventhLegion = 'maybe')
-AND (MonumentType = 'stela'
-OR MonumentType = 'titulus'
-OR MonumentType = 'funerary inscription'
-OR MonumentType = 'altar'
-OR MonumentType = 'sacral inscription'
-OR MonumentType = 'inscription fragment')
+AND (MemberSeventhLegion = 'maybe' or MemberSeventhLegion = 'yes')
+AND MonumentType IS NOT 'tegula' 
+AND MonumentType IS NOT 'boundary inscription' 
+AND MonumentType IS NOT 'construction dedication'
+AND MonumentType IS NOT 'sacral monument'
+AND MonumentType IS NOT 'sacral altar'
 GROUP BY Settlement
